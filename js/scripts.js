30b8ef1da15f2185c3e0258ef8195733f7514cb8
Melissa <100547690+MelissaLopezux@users.noreply.github.com>
2022-03-22 20:37:11 -0700

    edit class names to fix typos

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ console.log("Welcome to my page!");
 
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
-  if (x.className === 'navToggle'){
+  if (x.className === 'navtoggle'){
   x.className += ' responsive' ;
 } else {
   x.className = ' navtoggle' ;
